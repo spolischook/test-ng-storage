6b63a3e71e169d88d18b97ee5cdab863bce950ad
Serhii Polishchuk <spolischook@gmail.com>
2019-03-15 07:31:15 +0200

    Added storage test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { LocalStorage } from '@ngx-pwa/local-storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testStore';
+  constructor(protected localStorage: LocalStorage) {}
+  ngOnInit() {
+    this.localStorage.setItem('foo', 'bar').subscribe(() => {
+      this.localStorage.getItem('foo').subscribe(data => {
+        console.log('The foo is: ', data);
+      });
+    });
+  }
 }
